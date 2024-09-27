7914e93c4bd7c219e395255ac30270b50d9dd4c2
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-09-27 00:34:21 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def main():
-    pass
+import sys
+    
+def get_user_input():
+    return (sys.argv[1], sys.argv[2])
 
+def add_numbers(num1, num2):
+    return int(num1) + int(num2)
+
+def main():
+    num1, num2 = get_user_input()
+    print(add_numbers(num1, num2))
+    pass
 main()
